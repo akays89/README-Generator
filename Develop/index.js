9286eba98a4cb0cbe653ad8510fd23bd8b67705d
Amanda Kays <amanda@Amandas-MacBook-Air.local>
2021-07-08 22:51:38 -0500

    generate markdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,8 @@
-// TODO: Include packages needed for this application
+
 const inquirer = require("inquirer")
 const generateMarkdown = require("./utils/generateMarkdown.js")
 const fs = require("fs")
-// TODO: Create an array of questions for user input
+
 const questions = [
     {
         type: 'input',
@@ -47,24 +47,22 @@ const questions = [
 
     },
     {
-        type: 'checkbox',
+        type: 'list',
         name: 'license',
         message: 'Which license should be used?',
-        choices: [
-            'MIT', 'Apache', 'GLP', 'BSD',
-        ],
+        choices: ["Apache 2.0", "GNU GPL v2","MIT", "No License"]
     },
 
 ];
 
-// TODO: Create a function to write README file
+
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err)=> {
         err?console.error(err):console.log('Thanks!')
     })
 }
 
-// TODO: Create a function to initialize app
+
 function init() {
     inquirer
     .prompt(questions)
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,44 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  if(license ==="Apache 2.0") {
+    return `![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`;
+  } 
+  else if (license === "GNU GPL v2") {
+    return `![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)`;
+  }
+  else if (license === "MIT") {
+    return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+  }
+  else (license === "No License") 
+    return ( ``)
 
-// TODO: Create a function to generate markdown for README
+}
+
+
 function generateMarkdown(data) {
-  return `# ${data.title}
 
+return `
+# ${data.title}
+${renderLicenseBadge(data.license)}
+## Description
+${data.description}
+## Installation
+ ${data.installation}
+## Usage
+${data.usage}
+## Contributions
+${data.contribution}
+## Test
+${data.test}
+## License
+${data.license}
+${renderLicenseBadge(data.license)}
+## Contact
+Please feel free to contact me at ${data.email} for questions or feedback. 
+To see other projects, please visit https://github.com/${data.username}/.
 `;
 }
 
 module.exports = generateMarkdown;
+
